a585a361de30affc900f549d68955786c69f9dc8
Kosisochii1 <emmanuelezeoyirkosi@yahoo.com>
2024-09-17 10:03:08 +0100

    update

--- controllers/jobSeekerController.js
+++ controllers/jobSeekerController.js
@@ -8,6 +8,7 @@ const createJobSeeker = async (req, res) => {
   const userId = req.user._id;
 
   // Ensure user exists
+
   const user = await User.findById(userId);
   if (!user) {
     return res.status(404).json({ msg: "User not found" });

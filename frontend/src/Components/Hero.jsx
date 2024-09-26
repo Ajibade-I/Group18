7f8f10e1e4eb180de781d380afab463caba385f6
veramarach <edemahd@gmail.com>
2024-09-25 18:26:31 -0700

    animation added

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="bg-covers  bg-no-repeat bg-cover h-dvh flex px-14  items-center   tablet:justify-center ">
        <div className="mt-20 tablet:mt-0"> 
-       <h2 className=" font-extrabold  tablet:pt-14 montserrat-regular tablet:text-3xl  tablet:w-[220px] text-7xl text-white w-[850px]   ">Find The Best Startup Job That Fit You</h2>
+       <h2 className=" font-extrabold  tablet:pt-14 montserrat-regular tablet:text-3xl  tablet:w-[220px] text-7xl text-white w-[850px] animate-slide-down   ">Find The Best Startup Job That Fit You</h2>
        <p className="text-white w-[400px] py-4 tablet:w-full"> Whether you are an experienced professional or just starting out,our platform connects you with top employers across diverse industries.
        Discover the job that inspires and challenges you every day. Start your search now!
         </p>
